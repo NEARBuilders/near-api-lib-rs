809b1c3d5b56a7a8662f16662fe26bb87b002e81
Kshitij Chaurasiya <kshitij.dean.sam@gmail.com>
2024-02-14 00:12:52 +0530

    Feature: Add provider functions (send_transaction_async, tx_status, chunk, block, validators)

--- providers/src/provider.rs
+++ providers/src/provider.rs
@@ -1,10 +1,17 @@
 use async_trait::async_trait;
 use near_jsonrpc_client::methods::status::RpcStatusResponse;
 use near_jsonrpc_primitives::types::status::RpcStatusError;
-use near_primitives::views::FinalExecutionOutcomeView;
+use near_primitives::views::{FinalExecutionOutcomeView, ChunkView, BlockView, EpochValidatorInfo};
 use near_primitives::transaction::SignedTransaction;
 use near_jsonrpc_primitives::types::transactions::RpcTransactionError;
 use near_jsonrpc_client::errors::JsonRpcError;
+use near_jsonrpc_client::methods;
+use near_primitives::hash::CryptoHash;
+use near_primitives::types::{BlockReference, EpochReference};
+use near_jsonrpc_primitives::types::transactions::TransactionInfo;
+use near_jsonrpc_primitives::types::chunks::{RpcChunkError,  ChunkReference};
+use near_jsonrpc_primitives::types::blocks::RpcBlockError;
+use near_jsonrpc_primitives::types::validator::RpcValidatorError;
 
 
 // To-do
@@ -31,4 +38,9 @@ use near_jsonrpc_client::errors::JsonRpcError;
 pub trait Provider {
     async fn status(&self) -> Result<RpcStatusResponse, JsonRpcError<RpcStatusError>>;
     async fn send_transaction(&self, signed_transaction: SignedTransaction) -> Result<FinalExecutionOutcomeView, JsonRpcError<RpcTransactionError>>;
+    async fn send_transaction_async(&self, signed_transaction: SignedTransaction) -> Result<CryptoHash, JsonRpcError<methods::broadcast_tx_async::RpcBroadcastTxAsyncError>>;
+    async fn tx_status(&self, transaction_info: TransactionInfo) -> Result<FinalExecutionOutcomeView, JsonRpcError<RpcTransactionError>>;
+    async fn chunk(&self, chunk_reference: ChunkReference) -> Result<ChunkView, JsonRpcError<RpcChunkError>>;
+    async fn block(&self, block_reference: BlockReference) -> Result<BlockView, JsonRpcError<RpcBlockError>>;
+    async fn validators(&self, epoch_reference: EpochReference) -> Result<EpochValidatorInfo, JsonRpcError<RpcValidatorError>>;
 }
