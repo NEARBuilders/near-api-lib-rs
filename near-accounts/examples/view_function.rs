8ffc0bdf3abc9ebbad27298e90f6f59586fdab9e
jaswinder6991 <jassmultani2@gmail.com>
2024-05-20 22:51:57 +0530

    added types for Arc Signer and Provider, added view function support for multi-thread

--- near-accounts/examples/view_function.rs
+++ near-accounts/examples/view_function.rs
@@ -5,10 +5,7 @@ mod utils;
 use near_primitives::types::AccountId;
 use serde_json::json;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    env_logger::init();
-
+async fn single_thread() -> Result<(), Box<dyn std::error::Error>> {
     let contract_id: AccountId = "contract.near-api-rs.testnet".parse::<AccountId>()?;
 
     let provider = Arc::new(JsonRpcProvider::new("https://rpc.testnet.near.org"));
@@ -19,6 +16,37 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let result = view_function(provider, contract_id, method_name, args_json).await;
 
     println!("response: {:#?}", result);
+
+    Ok(())
+}
+
+async fn multi_thread() -> Result<(), Box<dyn std::error::Error>> {
+    let contract_id: AccountId = "contract.near-api-rs.testnet".parse::<AccountId>()?;
+
+    let provider = Arc::new(JsonRpcProvider::new("https://rpc.testnet.near.org"));
+
+    let args_json = json!({"account_id": "contract.near-api-rs.testnet"});
+    let method_name = "get_status".to_string();
+
+    let handle = tokio::spawn(async move {
+        let result = view_function(provider, contract_id, method_name, args_json).await;
+        println!("response: {:#?}", result);
+    });
+
+    // You can do more work here or wait for the handle if needed
+    handle.await?;
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
+
+    println!("Running single thread view function...");
+    single_thread().await?;
+
+    println!("Running multi thread view function...");
+    multi_thread().await?;
 
     Ok(())
 }
